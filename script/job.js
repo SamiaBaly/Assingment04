5e94ab8f2780ff8fd0bd7ee6df05356bedf35099
SamiaBaly <samiabaly116@gmail.com>
2026-02-21 23:56:14 +0600

    first commit

---
+++ script/job.js
@@ -0,0 +1,6 @@
+console.log("hello js");
+
+let totalCount = document.getElementById("total-count");
+console.log(totalCount.innerText);
+let cardCount = document.getElementById("card-count");
+console.log(cardCount.length)
