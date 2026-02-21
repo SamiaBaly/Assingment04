838870199da1ae6fce76da767da6d578bcd30af6
SamiaBaly <samiabaly116@gmail.com>
2026-02-22 00:33:23 +0600

    update js

--- script/job.js
+++ script/job.js
@@ -1,14 +1,27 @@
 
 let interviewList = [];
 let rejectList = [];
+
+
 let totalCount = document.getElementById("total-count");
 let interviewCount = document.getElementById("interview-count");
 let rejectedCount = document.getElementById("rejected-count");
 
+
 let cardCount = document.getElementById("card-count");
+const mainContainer = document.querySelector("main");
+console.log(mainContainer);
 
 
 function calculateCount() {
   totalCount.innerText = cardCount.children.length;
+  interviewCount.innerText = interviewList.length;
+  rejectedCount.innerText = rejectList.length; 
 }
 calculateCount();
+
+
+
+function toggleStyle(id) {
+  console.log("clicked")
+}
